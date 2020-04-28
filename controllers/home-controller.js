77774f4336cb1d1b44e60fd0e50c8916e489c38a
vighnesh153 <vighnesh.raut13@gmail.com>
2020-04-28 13:31:52 +0530

    add the home page route

---
+++ controllers/home-controller.js
@@ -0,0 +1,4 @@
+module.exports = (req, res) => {
+  res.send("<h1>This is my personal service. Please don't try to use it. I have set" +
+      " some mechanisms to not allow you to use this service. </h1>")
+};
--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const homePageController = require('./controllers/home-controller');
 const statusController = require('./controllers/status-controller');
 const sendMailController = require('./controllers/send-mail-controller');
 const _404Controller = require('./controllers/404-controller');
 
+router.get('/', homePageController);
 router.get('/status', statusController);
 router.post('/send', sendMailController);
 router.all('*', _404Controller);
